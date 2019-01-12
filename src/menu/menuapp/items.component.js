792e1b50cf6ebefa04cc05e2094a74588be53800
Fenghe Xu <jxphxufh@gmail.com>
2019-01-13 00:33:09 +0800

    fix: a missing close quotation mark

--- src/menu/menuapp/items.component.js
+++ src/menu/menuapp/items.component.js
@@ -1,7 +1,7 @@
 (function () {
   angular.module('MenuApp')
   .component('items', {
-    templateUrl: 'src/menu/menuapp/templates/items.template.html
+    templateUrl: 'src/menu/menuapp/templates/items.template.html',
     controller: 'ItemsController as items',
     bindings: {
       itemsList: '<'
